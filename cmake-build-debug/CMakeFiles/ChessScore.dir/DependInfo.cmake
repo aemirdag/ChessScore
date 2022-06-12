31c573f4f32e6a799c1638d865ae4269c08db40e
aemirdag <aaemirdag@gmail.com>
2022-06-12 16:09:08 +0300

    build

---
+++ cmake-build-debug/CMakeFiles/ChessScore.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Legion/Desktop/cs-tech/assignment/ChessScore/Pieces/Chessboard/Chessboard.cpp" "CMakeFiles/ChessScore.dir/Pieces/Chessboard/Chessboard.cpp.obj" "gcc" "CMakeFiles/ChessScore.dir/Pieces/Chessboard/Chessboard.cpp.obj.d"
+  "C:/Users/Legion/Desktop/cs-tech/assignment/ChessScore/Pieces/Chessboard/ThreatStrategy/BishopThreatStrategy.cpp" "CMakeFiles/ChessScore.dir/Pieces/Chessboard/ThreatStrategy/BishopThreatStrategy.cpp.obj" "gcc" "CMakeFiles/ChessScore.dir/Pieces/Chessboard/ThreatStrategy/BishopThreatStrategy.cpp.obj.d"
+  "C:/Users/Legion/Desktop/cs-tech/assignment/ChessScore/Pieces/Chessboard/ThreatStrategy/KingThreatStrategy.cpp" "CMakeFiles/ChessScore.dir/Pieces/Chessboard/ThreatStrategy/KingThreatStrategy.cpp.obj" "gcc" "CMakeFiles/ChessScore.dir/Pieces/Chessboard/ThreatStrategy/KingThreatStrategy.cpp.obj.d"
+  "C:/Users/Legion/Desktop/cs-tech/assignment/ChessScore/Pieces/Chessboard/ThreatStrategy/KnightThreatStrategy.cpp" "CMakeFiles/ChessScore.dir/Pieces/Chessboard/ThreatStrategy/KnightThreatStrategy.cpp.obj" "gcc" "CMakeFiles/ChessScore.dir/Pieces/Chessboard/ThreatStrategy/KnightThreatStrategy.cpp.obj.d"
+  "C:/Users/Legion/Desktop/cs-tech/assignment/ChessScore/Pieces/Chessboard/ThreatStrategy/PawnThreatStrategy.cpp" "CMakeFiles/ChessScore.dir/Pieces/Chessboard/ThreatStrategy/PawnThreatStrategy.cpp.obj" "gcc" "CMakeFiles/ChessScore.dir/Pieces/Chessboard/ThreatStrategy/PawnThreatStrategy.cpp.obj.d"
+  "C:/Users/Legion/Desktop/cs-tech/assignment/ChessScore/Pieces/Chessboard/ThreatStrategy/QueenThreatStrategy.cpp" "CMakeFiles/ChessScore.dir/Pieces/Chessboard/ThreatStrategy/QueenThreatStrategy.cpp.obj" "gcc" "CMakeFiles/ChessScore.dir/Pieces/Chessboard/ThreatStrategy/QueenThreatStrategy.cpp.obj.d"
+  "C:/Users/Legion/Desktop/cs-tech/assignment/ChessScore/Pieces/Chessboard/ThreatStrategy/RookThreatStrategy.cpp" "CMakeFiles/ChessScore.dir/Pieces/Chessboard/ThreatStrategy/RookThreatStrategy.cpp.obj" "gcc" "CMakeFiles/ChessScore.dir/Pieces/Chessboard/ThreatStrategy/RookThreatStrategy.cpp.obj.d"
+  "C:/Users/Legion/Desktop/cs-tech/assignment/ChessScore/Pieces/Chessman/Bishop.cpp" "CMakeFiles/ChessScore.dir/Pieces/Chessman/Bishop.cpp.obj" "gcc" "CMakeFiles/ChessScore.dir/Pieces/Chessman/Bishop.cpp.obj.d"
+  "C:/Users/Legion/Desktop/cs-tech/assignment/ChessScore/Pieces/Chessman/Chessman.cpp" "CMakeFiles/ChessScore.dir/Pieces/Chessman/Chessman.cpp.obj" "gcc" "CMakeFiles/ChessScore.dir/Pieces/Chessman/Chessman.cpp.obj.d"
+  "C:/Users/Legion/Desktop/cs-tech/assignment/ChessScore/Pieces/Chessman/King.cpp" "CMakeFiles/ChessScore.dir/Pieces/Chessman/King.cpp.obj" "gcc" "CMakeFiles/ChessScore.dir/Pieces/Chessman/King.cpp.obj.d"
+  "C:/Users/Legion/Desktop/cs-tech/assignment/ChessScore/Pieces/Chessman/Knight.cpp" "CMakeFiles/ChessScore.dir/Pieces/Chessman/Knight.cpp.obj" "gcc" "CMakeFiles/ChessScore.dir/Pieces/Chessman/Knight.cpp.obj.d"
+  "C:/Users/Legion/Desktop/cs-tech/assignment/ChessScore/Pieces/Chessman/Pawn.cpp" "CMakeFiles/ChessScore.dir/Pieces/Chessman/Pawn.cpp.obj" "gcc" "CMakeFiles/ChessScore.dir/Pieces/Chessman/Pawn.cpp.obj.d"
+  "C:/Users/Legion/Desktop/cs-tech/assignment/ChessScore/Pieces/Chessman/Queen.cpp" "CMakeFiles/ChessScore.dir/Pieces/Chessman/Queen.cpp.obj" "gcc" "CMakeFiles/ChessScore.dir/Pieces/Chessman/Queen.cpp.obj.d"
+  "C:/Users/Legion/Desktop/cs-tech/assignment/ChessScore/Pieces/Chessman/Rook.cpp" "CMakeFiles/ChessScore.dir/Pieces/Chessman/Rook.cpp.obj" "gcc" "CMakeFiles/ChessScore.dir/Pieces/Chessman/Rook.cpp.obj.d"
+  "C:/Users/Legion/Desktop/cs-tech/assignment/ChessScore/main.cpp" "CMakeFiles/ChessScore.dir/main.cpp.obj" "gcc" "CMakeFiles/ChessScore.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ChessScore.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ChessScore.
+# This may be replaced when dependencies are built.
