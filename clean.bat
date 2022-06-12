eb427d00362e39f89594f4b174467554bf7c0e71
aemirdag <aaemirdag@gmail.com>
2022-06-12 18:18:56 +0300

    devops

--- clean.bat
+++ clean.bat
@@ -2,9 +2,12 @@
 
 TITLE Chess Score Clean
 
+ECHO starting clean...
 ECHO ============================
 ECHO removing build directory...
 rmdir /S build
+ECHO ============================
+ECHO clean finished.
 ECHO ============================
 
 PAUSE
